b7ceaf1d33d46a776687a82192d5467261095735
ThomasPlaindoux <tomplaindoux1@orange.fr>
2018-11-05 14:00:39 +0000

    sort all pokemons

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -28,24 +28,57 @@ class PokemonController extends AbstractController {
     $params                  = array () ;
     $file                    = "csv/PokemonFr.csv" ;
     $allPokemon              = array () ;
+    $firstGen                = array () ;
     $handle                  = fopen ($file, "r") ;
     $bool                    = file_exists ($file) ;
     $title                   = fgetcsv ($handle) ;
+    $count                   = 0 ;
     while ($csv = fgetcsv ($handle)) {
       if (    is_null ($csv [1])
            || ! strlen ($csv [1])
          ) {
         $allPokemon        [count ($allPokemon)] =
-              array (           "id" => $csv [0]
+              array (           "id" => $this -> formatId ($csv [0])
                       ,       "name" => $csv [2]
                       , "generation" => $csv [3]
-                      ,      "type1" => $csv [4]
-                      ,      "type2" => $csv [5]
+                      ,      "type1" => strtolower ($csv [4])
+                      ,      "type2" => strtolower ($csv [5])
                     ) ;
+        if ($csv [3] == 1) {
+          $firstGen          [count ($firstGen)] =
+              array (           "id" => $this -> formatId ($csv [0])
+                      ,       "name" => $csv [2]
+                      , "generation" => $csv [3]
+                      ,      "type1" => strtolower ($csv [4])
+                      ,      "type2" => strtolower ($csv [5])
+                    ) ;
+        }
       }
+      $count++ ;
     }
-    echo "File (".count ($allPokemon).") : <pre>".print_r ($allPokemon, true)."</pre><br>" ;
+    // echo "File (".count ($allPokemon).") : <pre>".print_r ($allPokemon, true)."</pre><br>" ;
     fclose ($handle) ;
-    return $this -> render ('pokemon/index.html.twig', $params) ;
+    $ordered                 = $this -> sortArray ($allPokemon) ;
+    //echo "Ordered (".count ($ordered).") : <pre>".print_r ($ordered, true)."</pre><br>" ;
+    $params     ["pokemons"] = $ordered ;
+    return $this -> render ('pokemon/all.html.twig', $params) ;
+  }
+  protected function formatId ($id) {
+    return (   $id < 10
+             ? "00".$id
+             : (   $id < 100
+                 ? "0".$id
+                 : $id
+               )
+           ) ;
+  }
+  protected function sortArray ($array) {
+    $arrayTmp                = array () ;
+    for ($i = 0 ; $i < count ($array) ; $i++) {
+      $index                 = intval ($array [$i]["id"]) ;
+      $arrayTmp [$index]     = $array [$i] ;
+    }
+    ksort ($arrayTmp) ;
+    return $arrayTmp ;
   }
 }
